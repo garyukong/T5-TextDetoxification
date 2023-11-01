77d3e262bf46bb45f704912e7ef8ed8e3e902214
Gary Kong <garykong@berkeley.edu>
2023-11-01 12:26:18 +0000

    Finished classification-guided loss function

--- src/old_code.py
+++ src/old_code.py
@@ -271,3 +271,175 @@ def evaluate_metrics(
         results["BLEURT"] = bleurt
 
     return results
+
+class Seq2SeqTrainerCustomLoss(Seq2SeqTrainer):
+    def __init__(self, tokenizer_toxicity, model_toxicity, *args, **kwargs):
+        super(Seq2SeqTrainerCustomLoss, self).__init__(*args, **kwargs)
+        self.tokenizer_toxicity = tokenizer_toxicity
+        self.model_toxicity = model_toxicity.to(DEVICE)
+
+    def prediction_step_custom(
+        self,
+        model: nn.Module,
+        inputs: Dict[str, Union[torch.Tensor, Any]],
+        prediction_loss_only: bool,
+        ignore_keys: Optional[List[str]] = None,
+        **gen_kwargs,
+    ) -> Tuple[Optional[float], Optional[torch.Tensor], Optional[torch.Tensor]]:
+        """
+        Perform an evaluation step on `model` using `inputs`.
+
+        Subclass and override to inject custom behavior.
+
+        Args:
+            model (`nn.Module`):
+                The model to evaluate.
+            inputs (`Dict[str, Union[torch.Tensor, Any]]`):
+                The inputs and targets of the model.
+
+                The dictionary will be unpacked before being fed to the model. Most models expect the targets under the
+                argument `labels`. Check your model's documentation for all accepted arguments.
+            prediction_loss_only (`bool`):
+                Whether or not to return the loss only.
+            gen_kwargs:
+                Additional `generate` specific kwargs.
+
+        Return:
+            Tuple[Optional[float], Optional[torch.Tensor], Optional[torch.Tensor]]: A tuple with the loss, logits and
+            labels (each being optional).
+        """
+
+        if not self.args.predict_with_generate or prediction_loss_only:
+            return super().prediction_step(
+                model, inputs, prediction_loss_only=prediction_loss_only, ignore_keys=ignore_keys
+            )
+
+        has_labels = "labels" in inputs
+        inputs = self._prepare_inputs(inputs)
+
+        # XXX: adapt synced_gpus for fairscale as well
+        # Priority (handled in generate):
+        # non-`None` gen_kwargs > model.generation_config > default GenerationConfig()
+        if len(gen_kwargs) == 0 and hasattr(self, "_gen_kwargs"):
+            gen_kwargs = self._gen_kwargs.copy()
+        if "num_beams" in gen_kwargs and gen_kwargs["num_beams"] is None:
+            gen_kwargs.pop("num_beams")
+        if "max_length" in gen_kwargs and gen_kwargs["max_length"] is None:
+            gen_kwargs.pop("max_length")
+
+        gen_kwargs["synced_gpus"] = (
+            gen_kwargs["synced_gpus"] if gen_kwargs.get("synced_gpus") is not None else False
+        )
+
+        generation_inputs = inputs.copy()
+        # If the `decoder_input_ids` was created from `labels`, evict the former, so that the model can freely generate
+        # (otherwise, it would continue generating from the padded `decoder_input_ids`)
+        if (
+            "labels" in generation_inputs
+            and "decoder_input_ids" in generation_inputs
+            and generation_inputs["labels"].shape == generation_inputs["decoder_input_ids"].shape
+        ):
+            generation_inputs = {k: v for k, v in inputs.items() if k != "decoder_input_ids"}
+        generated_tokens = self.model.generate(**generation_inputs, **gen_kwargs)
+
+        # Temporary hack to ensure the generation config is not initialized for each iteration of the evaluation loop
+        # TODO: remove this hack when the legacy code that initializes generation_config from a model config is
+        # removed in https://github.com/huggingface/transformers/blob/98d88b23f54e5a23e741833f1e973fdf600cc2c5/src/transformers/generation/utils.py#L1183
+        if self.model.generation_config._from_model_config:
+            self.model.generation_config._from_model_config = False
+
+        # Retrieves GenerationConfig from model.generation_config
+        gen_config = self.model.generation_config
+        # in case the batch is shorter than max length, the output should be padded
+        if generated_tokens.shape[-1] < gen_config.max_length:
+            generated_tokens = self._pad_tensors_to_max_len(generated_tokens, gen_config.max_length)
+        elif gen_config.max_new_tokens is not None and generated_tokens.shape[-1] < gen_config.max_new_tokens + 1:
+            generated_tokens = self._pad_tensors_to_max_len(generated_tokens, gen_config.max_new_tokens + 1)
+
+        if has_labels:
+            with self.compute_loss_context_manager():
+                outputs = model(**inputs)
+            if self.label_smoother is not None:
+                loss = self.label_smoother(outputs, inputs["labels"]).mean().detach()
+            else:
+                loss = (outputs["loss"] if isinstance(outputs, dict) else outputs[0]).mean().detach()
+        else:
+            loss = None
+
+        if self.args.prediction_loss_only:
+            return loss, None, None
+
+        if has_labels:
+            labels = inputs["labels"]
+            if labels.shape[-1] < gen_config.max_length:
+                labels = self._pad_tensors_to_max_len(labels, gen_config.max_length)
+            elif gen_config.max_new_tokens is not None and labels.shape[-1] < gen_config.max_new_tokens + 1:
+                labels = self._pad_tensors_to_max_len(labels, gen_config.max_new_tokens + 1)
+        else:
+            labels = None
+
+        return loss, generated_tokens, labels
+
+    def classifier_guided_loss(self, generated_text, target_label):
+        # Tokenize the generated text
+        inputs = self.tokenizer_toxicity(generated_text, return_tensors="pt", padding=True)
+        
+        # Move inputs to the same device as the model
+        inputs = {key: tensor.to(DEVICE) for key, tensor in inputs.items()}
+        
+        # Get the batch size
+        batch_size = inputs["input_ids"].shape[0]
+
+        # Ensure target label is a tensor and on the correct device and shape
+        target_label = torch.tensor([target_label]*batch_size, dtype=torch.long).to(DEVICE)
+
+        # Get the classifier's output logits
+        logits = self.model_toxicity(**inputs)["logits"]
+        
+        # Calculate the cross-entropy loss
+        loss_fct = torch.nn.CrossEntropyLoss().to(DEVICE)
+        loss = loss_fct(logits, target_label)
+        
+        return loss
+    
+    def compute_loss(self, model, inputs):
+        """
+        Compute custom loss for the model.
+
+        Args:
+            model (torch.nn.Module): The model training or evaluating.
+            inputs (dict): The inputs and targets of the model.
+
+        Returns:
+            torch.FloatTensor: The loss value.
+        """
+        # # Get outputs, predictions and prediction loss
+        # outputs = model(**inputs)
+        # pred_loss = outputs.loss if isinstance(outputs, dict) else outputs[0]
+
+        # Call prediction_step
+        pred_loss, preds, refs = self.prediction_step_custom(model, inputs, prediction_loss_only=False)
+        
+        # Post-process the predictions and references
+        ## In case the model returns more than the prediction logits
+        if isinstance(preds, tuple):
+            preds = preds[0]
+
+        decoded_preds = self.tokenizer.batch_decode(preds, skip_special_tokens=True)
+
+        ## Replace -100s in the labels as we can't decode them
+        refs = torch.where(refs != -100, refs, torch.tensor(self.tokenizer.pad_token_id).to(DEVICE))
+        decoded_refs = self.tokenizer.batch_decode(refs, skip_special_tokens=True)
+
+        ## Some simple post-processing
+        decoded_preds = [pred.strip() for pred in decoded_preds]
+        decoded_refs = [ref.strip() for ref in decoded_refs]
+
+        # Calculate the classifier-guided loss
+        classifier_loss = self.classifier_guided_loss(decoded_preds, 0)
+
+        # Calculate total loss as a weighted sum of the classifier loss and the prediction loss
+        total_loss = 0.5 * classifier_loss + 0.5 * pred_loss
+
+        # Return total loss
+        return total_loss
